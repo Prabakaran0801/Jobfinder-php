1a96548330fc46fb8ad87051ac7e14a94f386d92
Prabakaran.M <karanpk0108@gmail.com>
2024-01-23 12:42:35 +0530

    Splited UI into partials

--- helper.php
+++ helper.php
@@ -4,7 +4,38 @@
  *@param string $path
  */
 
-function basePath($path = '')
-{
+function basePath($path = ''){
     return __DIR__ . '/' . $path;
+}
+
+/*
+*Load a view
+*@param string $name
+*@return void
+*/
+
+function lodeView($name){
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if(file_exists($viewPath)){
+        require $viewPath;
+    }else{
+        echo"View'{$name} not found!'";
+    }
+}
+
+/*
+*Load a partials
+*@param string $name
+*@return void
+*/
+
+function loadPartial($name){
+    $partialPath = basePath("views/partials/{$name}.php");
+    
+    if(file_exists($partialPath)){
+        require $partialPath;
+    }else{
+        echo"Partial'{$name} not found!'";
+    }
 }
