5aab1915eb82e4c36b1f2afdd39761ce7311f5c8
Prabakaran.M <karanpk0108@gmail.com>
2024-01-26 13:35:22 +0530

    Added custom auto loader

--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
 <?php
 require "../helper.php";
-require basePath("Framework/Router.php");
-require basePath("Framework/Database.php");
-
+ 
+spl_autoload_register(function ($class){
+    $path = basePath("Framework/" . $class . ".php");
+    if(file_exists($path)){
+        require $path;
+    }
+});
 
 //Itanciate the route
 $router = new Router();
