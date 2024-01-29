2e03b4b9f4330f01de39781646de1271ecc58206
Prabakaran.M <karanpk0108@gmail.com>
2024-01-29 17:38:59 +0530

    Implemented validation

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -75,6 +75,25 @@ class ListingController
         $newListingData["user_id"] = 1;
 
         $newListingData = array_map("sanitize", $newListingData);
-        inspectAndDie($newListingData);
+
+        $requiredFields = ["title", "description", "email", "city", "state"];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($newListingData[$field]) || !Validation::string($newListingData[$field])) {
+                $errors[$field] = ucfirst($field) . ' is requried';
+            }
+        }
+        if (!empty($errors)) {
+            //Reload view with errors
+            loadView("listings/create", [
+                "errors" => $errors,
+                "listings" => $newListingData
+            ]);
+        } else {
+            //Submit Data
+            echo "Submit was success!";
+        }
     }
 }
--- App/views/listings/create.view.php
+++ App/views/listings/create.view.php
@@ -15,46 +15,65 @@
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
             </h2>
+            <?php if (isset($errors)) : ?>
+            <?php foreach ($errors as $error) : ?>
+            <div class="message bg-red-500 my-3"><?= $error ?></div>
+            <?php endforeach; ?>
+            <?php endif; ?>
             <div class="mb-4">
-                <input type="text" name="title" placeholder="Job Title" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="title" placeholder="Job Title" value="<?= $listings["title"] ?? '' ?>"
+                    class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <textarea name="description" placeholder="Job Description" class="w-full px-4 py-2 border rounded focus:outline-none"></textarea>
+                <textarea name="description" placeholder="Job Description"
+                    class=" w-full px-4 py-2 border rounded focus:outline-none"><?= $listings["description"] ?? '' ?></textarea>
             </div>
             <div class="mb-4">
-                <input type="text" name="salary" placeholder="Annual Salary" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="salary" value="<?= $listings["salary"] ?? '' ?>" placeholder="Annual Salary"
+                    class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <input type="text" name="requirements" placeholder="Requirements" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="requirements" placeholder="Requirements"
+                    value="<?= $listings["requirements"] ?? '' ?>"
+                    class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <input type="text" name="benefits" placeholder="Benefits" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="benefits" placeholder="Benefits" value="<?= $listings["benefits"] ?? '' ?>"
+                    class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Company Info & Location
             </h2>
             <div class="mb-4">
-                <input type="text" name="company" placeholder="Company Name" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="company" placeholder="Company Name" value="<?= $listings["company"] ?? '' ?>"
+                    class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <input type="text" name="address" placeholder="Address" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="address" value="<?= $listings["address"] ?? '' ?>" placeholder="Address"
+                    class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <input type="text" name="city" placeholder="City" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="city" placeholder="City" value="<?= $listings["city"] ?? '' ?>"
+                    class=" w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <input type="text" name="state" placeholder="State" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="state" placeholder="State" value="<?= $listings["state"] ?? '' ?>"
+                    class=" w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <input type="text" name="phone" placeholder="Phone" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="text" name="phone" placeholder="Phone" value="<?= $listings["phone"] ?? '' ?>"
+                    class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
-                <input type="email" name="email" placeholder="Email Address For Applications" class="w-full px-4 py-2 border rounded focus:outline-none" />
+                <input type="email" name="email" placeholder="Email Address For Applications"
+                    value="<?= $listings["email"] ?? '' ?>"
+                    class=" w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <button class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
                 Save
             </button>
-            <a href="/" class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none">
+            <a href="/"
+                class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none">
                 Cancel
             </a>
         </form>
