c4e2782204c20626cf8d001231e50dc80877c4d6
Prabakaran.M <karanpk0108@gmail.com>
2024-01-30 19:53:19 +0530

    Added register validation and partial

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -34,4 +34,50 @@ class UserController
     {
         loadView('users/create');
     }
+
+    /**
+     *Store user in database
+     *
+     *@return void
+     */
+
+    public function store()
+    {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $city = $_POST["city"];
+        $state = $_POST["state"];
+        $password = $_POST["password"];
+        $passwordConformation = $_POST["password_confirmation"];
+
+        $errors = [];
+        //Validation
+        if (!Validation::email($email)) {
+            $errors['email'] = "Please enter vaild email";
+        }
+        if (!Validation::string($name, 2, 50)) {
+            $errors['name'] = "Name must be between 2-50 characters";
+        }
+        if (!Validation::string($password, 6, 50)) {
+            $errors['password'] = "Password must be at least 6 characters";
+        }
+        if (!Validation::match($password, $passwordConformation)) {
+            $errors['password_confirmation'] = "Passwords do not match";
+        }
+
+        if (!empty($errors)) {
+            loadView("users/create", [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state,
+                ]
+            ]);
+            exit;
+        } else {
+            inspectAndDie("store");
+        }
+    }
 }
